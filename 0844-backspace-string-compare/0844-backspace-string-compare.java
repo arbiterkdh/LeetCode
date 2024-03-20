113fa7515a70ece4747f8c8ffbef7cb945011515
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-20 11:44:15 +0900

    Time: 2 ms (60.02%), Space: 41.6 MB (40.64%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -5,22 +5,18 @@ class Solution {
         Stack<Character> stack1 = new Stack<>();
         Stack<Character> stack2 = new Stack<>();
         for(int i = 0; i<s.length(); i++){
-            stack1.push(s.charAt(i));
-            if(s.charAt(i)=='#' && stack1.size()==1){
+            if(s.charAt(i)=='#' && !stack1.empty()){
                 stack1.pop();
-            }else if(s.charAt(i)=='#'){
-                stack1.pop();
-                stack1.pop();
+            }else if(s.charAt(i)!='#'){
+                stack1.push(s.charAt(i));
             }
         }
 
         for(int i = 0; i<t.length(); i++){
-            stack2.push(t.charAt(i));
-            if(t.charAt(i)=='#' && stack2.size()==1){
+            if(t.charAt(i)=='#' && !stack2.empty()){
                 stack2.pop();
-            }else if(t.charAt(i)=='#'){
-                stack2.pop();
-                stack2.pop();
+            }else if(t.charAt(i)!='#'){
+                stack2.push(t.charAt(i));
             }
         }
 
