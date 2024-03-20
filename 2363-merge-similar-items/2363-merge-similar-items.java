d924e13f88d77945433e505fa0fcfd8182ca81fa
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2024-03-20 20:59:36 +0900

    Time: 3 ms (98.03%), Space: 45.4 MB (50.81%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.java
+++ 2363-merge-similar-items/2363-merge-similar-items.java
@@ -2,23 +2,19 @@ import java.util.List;
 
 class Solution {
     public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+        List<List<Integer>> ret = new ArrayList<>();
         int[] sum = new int[1001];
 
         for(int[] item : items1){
-            sum[item[0]]+=item[1];
+            sum[item[0]] += item[1];
         }
         for(int[] item : items2){
-            sum[item[0]]+=item[1];
+            sum[item[0]] += item[1];
         }
 
-        List<List<Integer>> ret = new ArrayList<>();
-
         for(int i=1; i<=1000; i++){
-            List<Integer> retElements = new ArrayList<>();
             if(sum[i]!=0){
-                retElements.add(i);
-                retElements.add(sum[i]);
-                ret.add(retElements);
+                ret.add(Arrays.asList(i,sum[i]));
             }
         }
         return ret;
